13ac587d671528356d43a76c781422bba9fa1b3f
Ricardo Gonzalez <geminis_richy@hotmail.com>
2026-03-24 00:25:19 -0300

    Uso de componente Link

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';   // permite la precarga de enlaces para mejorar la experiencia del usuario
+import { usePathname } from 'next/navigation'; // permite obtenr la ruta actual para marcar el enlace activo
+import clsx from 'clsx'; // permite combinar clases condicionalmente
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +22,25 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname(); // almaceno la ruta actual para marcar el enlace activo
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            {
+              'bg-sky-100 text-blue-600' : pathname === link.href, // marca el enlace activo comparando la ruta actual con el href del enlace
+            },
+          )}  
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
